25d40aa5e7ce3a2180249ee3e71092aa4526ad06
Lavanya Modukuru <lavanya.modukuru@gmail.com>
2025-09-20 12:20:04 +0530

    added new project

---
+++ Java_lab001/src/Table.java
@@ -0,0 +1,12 @@
+public class Table {
+    public static void main() {
+        int a=9;
+        int s;
+        for(int i=1;i<=10;i++)
+        {
+            s=a*i;
+            System.out.println("%d*%d=%d",a,i,s);
+
+        }
+    }
+}
